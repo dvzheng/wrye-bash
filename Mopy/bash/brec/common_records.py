99a9fb1f315d825b94b4208a8a5353c352e88743
Infernio <infernio@icloud.com>
2020-05-25 01:56:16 +0200

    Some minor changes for Linux compatibility
    Mostly getting rid of hardcoded backslash separators. I was going to add
    bass.dirs['db'], but turns out it already existed and was just unused.
    
    Doesn't launch without commenting out a huge chunk of env.py. If you do
    do that however, it is now even possible to build a BP.

--- Mopy/bash/brec/common_records.py
+++ Mopy/bash/brec/common_records.py
@@ -38,7 +38,7 @@ from .common_subrecords import MelEdid
 from .mod_io import RecordHeader, GrupHeader
 from .record_structs import MelRecord, MelSet, MreRecord
 from .utils_constants import FID
-from .. import bolt, exception
+from .. import bass, bolt, exception
 from ..bolt import decode, encode, GPath, sio
 from ..exception import StateError
 
@@ -210,9 +210,9 @@ class MreGmstBase(MelRecord):
         from .. import bosh # Late import to avoid circular imports
         if not cls.Ids:
             from .. import bush
-            fname = bush.game.pklfile
+            fname = bass.dirs[u'db'].join(bush.game.pklfile)
             try:
-                with open(fname) as pkl_file:
+                with fname.open(u'rb') as pkl_file:
                     cls.Ids = pickle.load(pkl_file)[cls.rec_sig]
             except:
                 old = bolt.deprintOn
