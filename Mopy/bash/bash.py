dvzheng/wrye-bash

commit e00d510be8a7741987b3f7e4b1325d4106b959cd

Removed all \n's from translatable strings
This can brick the resulting translation files completely:

msgid ""
msgstr "Project-Id-Version: PACKAGE VERSION
POT-Creation-Date: 2011-12-18 18:26+Alaskan Standard Time
PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE
Last-Translator: FULL NAME <EMAIL@ADDRESS>
Language-Team: LANGUAGE <LL@li.org>
MIME-Version: 1.0
Content-Type: text/plain; charset=cp1252
Content-Transfer-Encoding: utf8
Generated-By: pygettext.py 1.5
"
"Wrye Bash could not find a game to manage. Please use\n" # <--- offending \n is right here
"the -o command line argument to specify the game path."

instead of

msgid "Wrye Bash could not find a game to manage. Please use the -o command line argument to specify the game path."
msgstr ""

Either keep them out of the strings entirely or wrap with textwrap
(see change in bash.py).

Mopy/bash/basher/patcher_dialog.py:
That shouldn't be translatable

Mopy/bash/bash.py
@@ -449,18 +449,19 @@ def _import_bush_and_set_game(opts, bashIni):
     ret, game_icons = bush.detect_and_set_game(opts.oblivionPath, bashIni)
     if ret is not None:  # None == success
         if len(ret) == 0:
-            msgtext = _(
-                u"Wrye Bash could not find a game to manage. Please use\n"
-                u"the -o command line argument to specify the game path.")
+            msgtext = bolt.text_wrap(
+                _(u'Wrye Bash could not find a game to manage. Please use the '
+                  u'-o command line argument to specify the game path.'), 65)
         else:
-            msgtext = _(
-                u"Wrye Bash could not determine which game to manage.\n"
-                u"The following games have been detected, please select "
-                u"one to manage.")
-            msgtext += u'\n\n'
-            msgtext += _(
-                u'To prevent this message in the future, use the -o command\n'
-                u'line argument or the bash.ini to specify the game path.')
+            msgtext = (
+                    bolt.text_wrap(
+                        _(u'Wrye Bash could not determine which game to '
+                          u'manage. The following games have been detected, '
+                          u'please select one to manage.'), 65) + u'\n\n' +
+                    bolt.text_wrap(
+                        _(u'To prevent this message in the future, use the -o '
+                          u'command line argument or the bash.ini to specify '
+                          u'the game path.'), 65))
         retCode = _wxSelectGame(ret, game_icons, msgtext)
         if retCode is None:
             bolt.deprint(u'No games were found or selected. Aborting.')

Mopy/bash/basher/mod_links.py
@@ -133,11 +133,13 @@ class Mod_CreateDummyMasters(OneItemLink):
 
     def Execute(self):
         """Create Dummy Masters"""
-        msg = _(u'This is an advanced feature, originally intended for '
-                u"viewing and editing 'Filter' patches in %s. It will create "
-                u'empty plugins for each missing master. Are you sure you '
-                u'want to continue?\n\nTo remove these files later, use '
-                u"'Clean Dummy Masters...'") % bush.game.Xe.full_name
+        msg = (_(u'This is an advanced feature, originally intended for '
+                 u"viewing and editing 'Filter' patches in %s. It will create "
+                 u'empty plugins for each missing master. Are you sure you '
+                 u'want to continue?') + u'\n\n' +
+               _(u"To remove these files later, use 'Remove Dummy "
+                 u"Masters...'.")
+               % bush.game.Xe.full_name)
         if not self._askYes(msg, title=_(u'Create Files')): return
         to_refresh = []
         # creates esp files - so place them correctly after the last esm
@@ -237,8 +239,9 @@ class Mod_Move(EnabledLink):
             # will just lead to sadness
             entered_text = self._askText(
                 _(u'Please enter the plugin index to which the selected '
-                  u'plugins should be moved.\nNote that it must be a '
-                  u'hexadecimal number, as shown in the Mods tab.'),
+                  u'plugins should be moved.') + u'\n' +
+                _(u'Note that it must be a hexadecimal number, as shown in '
+                  u'the Mods tab.'),
                 default=u'%X' % default_index)
             if not entered_text: return # Abort if canceled or empty string
             target_index = int(entered_text, base=16)

Mopy/bash/basher/gui_patchers.py
@@ -1182,9 +1182,10 @@ class CBash_SpellsPatcher(_ASpellsPatcher):
 # Non CBash Importers----------------------------------------------------------
 class DestructiblePatcher(_ImporterPatcherPanel):
     patcher_name = _(u'Import Destructible')
-    _patcher_txt = _(u'Preserves changes to destructible records.\n\nWill '
-                     u'have to use if a mod that allows you to destroy part '
-                     u'of the environment is installed and active.')
+    _patcher_txt = (_(u'Preserves changes to destructible records.')
+                    + u'\n\n' +
+                    _(u'Will have to use if a mod that allows you to destroy '
+                      u'part of the environment is installed and active.'))
     autoKey = {u'Destructible'}
     patcher_type = preservers.DestructiblePatcher
 

Mopy/bash/basher/patcher_dialog.py
@@ -332,23 +332,28 @@ class PatchDialog(DialogWindow):
 
     def _pretry(self, patch_name):
         return balt.askYes(
-            self, _(u'Bash encountered an error when saving %(patch_name)s.'
-                    u'\n\nEither Bash needs Administrator Privileges to save '
-                    u'the file, or the file is in use by another process such '
-                    u'as %(xedit_name)s.\nPlease close any program that is '
-                    u'accessing %(patch_name)s, and provide Administrator '
-                    u'Privileges if prompted to do so.\n\nTry again?') % {
+            self, (_(u'Bash encountered an error when saving '
+                     u'%(patch_name)s.') + u'\n\n' +
+                   _(u'Either Bash needs Administrator Privileges to save '
+                     u'the file, or the file is in use by another process '
+                     u'such as %(xedit_name)s.') + u'\n' +
+                   _(u'Please close any program that is accessing '
+                     u'%(patch_name)s, and provide Administrator Privileges '
+                     u'if prompted to do so.') + u'\n\n' +
+                   _(u'Try again?')) % {
                 u'patch_name': patch_name.s,
                 u'xedit_name': bush.game.Xe.full_name},
             _(u'Bashed Patch - Save Error'))
 
     def _cretry(self, patch_name):
         return balt.askYes(
-            self, _(u'Bash encountered an error when renaming '
-                    u'%(temp_patch)s to %(patch_name)s.\n\nThe file is in use '
-                    u'by another process such as %(xedit_name)s.\nPlease '
-                    u'close the other program that is accessing %s.\n\nTry '
-                    u'again?') % {
+            self, (_(u'Bash encountered an error when renaming %(temp_patch)s '
+                     u'to %(patch_name)s.') + u'\n\n' +
+                   _(u'The file is in use by another process such as '
+                     u'%(xedit_name)s.') + u'\n' +
+                   _(u'Please close the other program that is accessing '
+                     u'%(patch_name)s.') + u'\n\n' +
+                   _(u'Try again?')) % {
                 u'temp_patch': patch_name.temp.s, u'patch_name': patch_name.s,
                 u'xedit_name': bush.game.Xe.full_name},
             _(u'Bashed Patch - Save Error'))
@@ -374,7 +379,7 @@ class PatchDialog(DialogWindow):
     __new_key = u'Saved Bashed Patch Configuration (%s)'
     def ImportConfig(self):
         """Import the configuration from a user selected dat file."""
-        config_dat = self.patchInfo.name + _(u'_Configuration.dat')
+        config_dat = self.patchInfo.name + u'_Configuration.dat'
         textDir = bass.dirs['patches']
         textDir.makedirs()
         #--File dialog

Mopy/bash/bosh/__init__.py
@@ -2353,11 +2353,11 @@ class ModInfos(FileInfos):
         """Return the list as wtxt of current bash tags (but don't say which
         ones are applied via a patch) - either for all mods in the data folder
         or if specified for one specific mod."""
-        tagList = u'=== '+_(u'Current Bash Tags')+u':\n'
+        tagList = u'=== ' + _(u'Current Bash Tags') + u':\n'
         tagList += u'[spoiler]\n'
         tagList += _(u'Note: Sources are processed from top to bottom, '
                      u'meaning that lower-ranking sources override '
-                     u'higher-ranking ones.\n')
+                     u'higher-ranking ones.') + u'\n'
         if mod_list:
             for modInfo in mod_list:
                 tagList += u'\n* ' + modInfo.name.s + u'\n'
@@ -2719,10 +2719,13 @@ class ModInfos(FileInfos):
 
     def _retry(self, old, new):
         return balt.askYes(
-            self, _(u'Bash encountered an error when renaming %(old)s to '
-                    u'%(new)s.\n\nThe file is in use by another process such '
-                    u'as %(xedit_name)s.\nPlease close the other program that '
-                    u'is accessing %(new)s.\n\nTry again?') % {
+            self, (_(u'Bash encountered an error when renaming %(old)s to '
+                    u'%(new)s.') + u'\n\n' +
+                   _(u'The file is in use by another process such as '
+                     u'%(xedit_name)s.') + u'\n' +
+                   _(u'Please close the other program that is accessing '
+                     u'%(new)s.') + u'\n\n' +
+                   _(u'Try again?')) % {
                 u'xedit_name': bush.game.Xe.full_name, u'old': old.s,
                 u'new': new.s},
         _(u'File in use'))

commit 9502f68694c1cc5b9407af4f11a180e8a4bb9dd8

Regression: Fix formatting errors from \n change
Broke in e00d510be8a7741987b3f7e4b1325d4106b959cd.
Took the opportunity to clean up the wrapping code in bash.py.

Mopy/bash/bash.py
@@ -449,20 +449,17 @@ def _import_bush_and_set_game(opts, bashIni):
     ret, game_icons = bush.detect_and_set_game(opts.oblivionPath, bashIni)
     if ret is not None:  # None == success
         if len(ret) == 0:
-            msgtext = bolt.text_wrap(
-                _(u'Wrye Bash could not find a game to manage. Please use the '
-                  u'-o command line argument to specify the game path.'), 65)
+            msgtext = _(u'Wrye Bash could not find a game to manage. Please '
+                        u'use the -o command line argument to specify the '
+                        u'game path.')
         else:
-            msgtext = (
-                    bolt.text_wrap(
-                        _(u'Wrye Bash could not determine which game to '
-                          u'manage. The following games have been detected, '
-                          u'please select one to manage.'), 65) + u'\n\n' +
-                    bolt.text_wrap(
-                        _(u'To prevent this message in the future, use the -o '
-                          u'command line argument or the bash.ini to specify '
-                          u'the game path.'), 65))
-        retCode = _wxSelectGame(ret, game_icons, msgtext)
+            msgtext = (_(u'Wrye Bash could not determine which game to '
+                         u'manage. The following games have been detected, '
+                         u'please select one to manage.') + u'\n\n' +
+                       _(u'To prevent this message in the future, use the -o '
+                         u'command line argument or the bash.ini to specify '
+                         u'the game path.'))
+        retCode = _wxSelectGame(ret, game_icons, bolt.text_wrap(msgtext, 65))
         if retCode is None:
             bolt.deprint(u'No games were found or selected. Aborting.')
             return None

Mopy/bash/basher/mod_links.py
@@ -136,10 +136,9 @@ class Mod_CreateDummyMasters(OneItemLink):
         msg = (_(u'This is an advanced feature, originally intended for '
                  u"viewing and editing 'Filter' patches in %s. It will create "
                  u'empty plugins for each missing master. Are you sure you '
-                 u'want to continue?') + u'\n\n' +
+                 u'want to continue?') % bush.game.Xe.full_name + u'\n\n' +
                _(u"To remove these files later, use 'Remove Dummy "
-                 u"Masters...'.")
-               % bush.game.Xe.full_name)
+                 u"Masters...'."))
         if not self._askYes(msg, title=_(u'Create Files')): return
         to_refresh = []
         # creates esp files - so place them correctly after the last esm